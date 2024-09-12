fbd2519720c268aba418f503c5e261f963093a72
Justl3est <111554501+justbest2003@users.noreply.github.com>
2024-09-12 16:05:14 +0700

    Update Cors!

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,10 @@ const app = express();
 const financialRouter = require("./router/financial.router");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+const frontend_url = process.env.FRONTEND_URL;
 const cors = require("cors");
 const corsOptions = {
-  origin: [
-    "https://financialtracker-backend-frontend.vercel.app",
-    "http://localhost:5173",
-  ],
+  origin: frontend_url,
 };
 
 //use Middleware
